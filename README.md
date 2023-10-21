ad17e25bc239c580cc995ab46d65cf653f149740
ladarat <ladarat.y@gmail.com>
2023-10-21 15:06:41 +0700

    Init Flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# fish_nugket_warehourse
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fish_nugket_warehourse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fish_nugket_warehourse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
