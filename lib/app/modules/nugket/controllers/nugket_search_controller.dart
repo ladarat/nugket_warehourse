f7a324328a626275397f5d06768dba525c938400
ladarat <ladarat.y@gmail.com>
2023-10-21 19:59:58 +0700

    get nugketDetails and add loading

--- lib/app/modules/nugket/controllers/nugket_search_controller.dart
+++ lib/app/modules/nugket/controllers/nugket_search_controller.dart
@@ -2,21 +2,35 @@ import 'package:fish_nugket_warehourse/app/data/model/location_request.dart';
 import 'package:fish_nugket_warehourse/app/data/model/warehouse_response.dart';
 import 'package:fish_nugket_warehourse/app/data/repository/nugket_repository.dart';
 import 'package:fish_nugket_warehourse/app/modules/nugket/models/nugket_detail_ui_data.dart';
+import 'package:fish_nugket_warehourse/app/network/exceptions/app_exception.dart';
 import 'package:get/get.dart';
 
 class NugketSearchController extends GetxController {
   NugketRepository _nugketRepository =
       Get.find(tag: (NugketRepository).toString());
 
-  final nugketDetails = <NugketDetailUiData>[].obs;
+  final _isLoading = true.obs;
+  final List<NugketDetailUiData> _nugketDetails = <NugketDetailUiData>[].obs;
+  bool get isLoading => _isLoading.value;
 
-  void searchNugket(LocationRequest locationRequest) async {
-    final result = await _nugketRepository.searchNugketNearBy(locationRequest);
-    convertToNugketDetailUiData(result);
+  List<NugketDetailUiData> get nugketDetails => _nugketDetails;
+
+  Future<void> fetchNugkets(LocationRequest locationRequest) async {
+    try {
+      _isLoading(true);
+      final result =
+          await _nugketRepository.searchNugketNearBy(locationRequest);
+      print(result);
+      convertToNugketDetailUiData(result);
+    } on AppException catch (e) {
+      print(e.message);
+    } finally {
+      _isLoading(false);
+    }
   }
 
   void convertToNugketDetailUiData(WarehouseResponse response) {
-    final _nugketDetails = <NugketDetailUiData>[];
+    final _nugketDetailList = <NugketDetailUiData>[];
     for (var store in response.data.warehouses.warehouseStores) {
       _nugketDetails.add(
         NugketDetailUiData(
@@ -29,6 +43,6 @@ class NugketSearchController extends GetxController {
       );
     }
 
-    nugketDetails.addAll(_nugketDetails);
+    _nugketDetails.addAll(_nugketDetailList);
   }
 }
